b0b6b5fecd25a20713f276a6ec720f36ecf2cd5d
Murilo Pereira <murilo@murilopereira.com>
2011-10-29 17:30:27 -0200

    Handle client registering.

--- src/werld_client_sup.erl
+++ src/werld_client_sup.erl
@@ -20,18 +20,20 @@ loop(ClientList) ->
       loop(ClientList);
     {event, Client} ->
       io:format("~p event ~w~n", [erlang:localtime(), Client#client.socket]),
-      NewClientList =
-        case werld_client_list:member(Client, ClientList) of
-          true ->
-            % FIXME: update player sendind event more inteligently.
-            [Client | werld_client_list:delete(Client, ClientList)];
-          false ->
-            io:format("~p adding ~s~n",
-                      [erlang:localtime(), Client#client.player#player.name]),
-            [Client | ClientList]
-        end,
-      gen_tcp:send(Client#client.socket, <<1>>),
-      loop(NewClientList);
+
+      PlayerList = werld_client_list:player_list(ClientList),
+      Payload = werld_player_list:to_binary(PlayerList),
+      PlayerListLength = length(PlayerList),
+      Data = <<PlayerListLength:4/native-unit:8, Payload/binary>>,
+      io:format("~p sending ~B bytes ~p~n",
+                [erlang:localtime(), length(binary_to_list(Data)), Data]),
+      [gen_tcp:send(S, Data) || S <- werld_client_list:socket_list(ClientList)],
+      % FIXME: update player state more intelligently.
+      loop([Client | werld_client_list:delete(Client, ClientList)]);
+    {register, Client} ->
+      io:format("~p registering ~s~n",
+                [erlang:localtime(), Client#client.player#player.name]),
+      loop([Client | ClientList]);
     {disconnect, Socket} ->
       io:format("~p disconnect ~w~n", [erlang:localtime(), Socket]),
       loop(lists:keydelete(Socket, 2, ClientList));
--- src/werld_sup.erl
+++ src/werld_sup.erl
@@ -33,6 +33,11 @@ loop(Socket) ->
       Client = #client{socket = Socket, player = Player},
       client_sup ! {event, Client},
       loop(Socket);
+    {tcp, Socket, <<"register", Id:4/bytes, Name:20/bytes, Y:4/bytes, X:4/bytes>>} ->
+      Player = #player{id = Id, name = Name, y = Y, x = X},
+      Client = #client{socket = Socket, player = Player},
+      client_sup ! {register, Client},
+      loop(Socket);
     {tcp, Socket, <<"get_players", _/binary>>} ->
       client_sup ! {Socket, {all}},
       loop(Socket);
