4b28ae28362f9517cf0ea8ae1bf19666b290c83c
Murilo Pereira <murilo@murilopereira.com>
2011-12-04 14:10:02 -0200

    Build walled world map.

--- src/werld_map.erl
+++ src/werld_map.erl
@@ -5,9 +5,19 @@
 % TODO: we're currently ignoring the map type. In the future we'll have more
 % than one type of map, so we'll need to build different maps.
 build(_) ->
-  Width = 60,
-  Height = 20,
-  Tiles = array:new(Width * Height, {default, 0}),
+  Width = 80,
+  Height = 22,
+  EmptyTiles = array:new(Width * Height, {default, ?WERLD_MAP_GROUND}),
+  WallTileIndexes = lists:seq(0, Height - 1) ++
+                      lists:seq((Width - 1) * Height, Width * Height - 1) ++
+                      lists:seq(Height, (Width - 1) * Height - 1, Height) ++
+                      lists:seq(2 * Height - 1, (Width - 1) * Height - 1, Height),
+  Tiles = array:map(fun(Index, Value) ->
+                      case lists:member(Index, WallTileIndexes) of
+                        true -> ?WERLD_MAP_WALL;
+                        false -> Value
+                      end
+                    end, EmptyTiles),
   #map{width = Width, height = Height, tiles = Tiles}.
 
 to_binary(Map) ->
