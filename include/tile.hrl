26ff9093e5f3e0d18d8fd01213ae366418051689
Murilo Pereira <murilo@murilopereira.com>
2011-12-10 19:59:15 -0200

    Moar tile types.

--- include/tile.hrl
+++ include/tile.hrl
@@ -1,6 +1,11 @@
 -record(tile, {type, properties}).
 
--define(WERLD_TILE_TYPE_GROUND, 0).
--define(WERLD_TILE_TYPE_WALL,   1).
+-define(WERLD_TILE_TYPE_DIRT,  0).
+-define(WERLD_TILE_TYPE_GRASS, 1).
+-define(WERLD_TILE_TYPE_SAND,  2).
+-define(WERLD_TILE_TYPE_SNOW,  3).
+-define(WERLD_TILE_TYPE_WATER, 4).
+-define(WERLD_TILE_TYPE_STONE, 4).
+-define(WERLD_TILE_TYPE_WALL,  6).
 
 -define(WERLD_TILE_PROPERTY_TRAVERSABLE, 2#00000001).
--- src/werld_map.erl
+++ src/werld_map.erl
@@ -8,7 +8,7 @@
 build(_) ->
   Width = 80,
   Height = 22,
-  EmptyTiles = array:new(Width * Height, {default, ?WERLD_TILE_TYPE_GROUND}),
+  EmptyTiles = array:new(Width * Height, {default, ?WERLD_TILE_TYPE_GRASS}),
   WallTileIndexes = lists:seq(0, Height - 1) ++
                       lists:seq((Width - 1) * Height, Width * Height - 1) ++
                       lists:seq(Height, (Width - 1) * Height - 1, Height) ++
@@ -17,7 +17,7 @@ build(_) ->
                       case lists:member(Index, WallTileIndexes) of
                         true -> <<?WERLD_TILE_TYPE_WALL:8/unsigned,
                                   0:8/unsigned>>;
-                        false -> <<?WERLD_TILE_TYPE_GROUND:8/unsigned,
+                        false -> <<?WERLD_TILE_TYPE_GRASS:8/unsigned,
                                    ?WERLD_TILE_PROPERTY_TRAVERSABLE:8/unsigned>>
                       end
                     end, EmptyTiles),
